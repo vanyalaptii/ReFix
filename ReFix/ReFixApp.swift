891278aeacb7d6f8ed83a2594ac6b77b294dfd22
vanyalaptii <vanya.lapty@gmail.com>
2023-12-17 22:27:38 +0200

    Initial Commit

---
+++ ReFix/ReFixApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReFixApp.swift
+//  ReFix
+//
+//  Created by Ваня Лаптий on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ReFixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
