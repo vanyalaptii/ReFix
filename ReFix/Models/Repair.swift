4ad0027d6be32872c35cdffe327ce9288d347d12
vanyalaptii <vanya.lapty@gmail.com>
2023-12-25 22:19:00 +0200

    Move moc to extention in Repairs model
    Move moc to extention in Repairs model

--- ReFix/Models/Repair.swift
+++ ReFix/Models/Repair.swift
@@ -34,3 +34,35 @@ class Repair: Identifiable {
         self.repairStatus = repairStatus
     }
 }
+
+extension Repair {
+    
+    static let repairsArray = [
+        Repair(
+            id: 1,
+            brand: "Apple",
+            model: "iPhone 13",
+            serialNumber: "MK35HF754",
+            imei: 432143214321123,
+            malfunction: "Швидко розряджається",
+            description: "Подряпини єкрану",
+            client: Client(id: 1, name: "Микола", phoneNumber: "09768459327", email: "testmail@gmail.com"),
+            telephone: "0976354213",
+            employee: "Світлана",
+            repairStatus: "Узгодження"),
+
+        Repair(
+            id: 2,
+            brand: "Apple",
+            model: "MacBook Pro 13",
+            serialNumber: "MK354SYR4",
+            imei: nil,
+            malfunction: "Розбитий екран",
+            description: "Потертості",
+            client: Client(id: 2, name: "Іван Федорович", phoneNumber: "0682686451", email: "testmail2@gmail.com"),
+            telephone: "0686754387",
+            employee: "Олена",
+            repairStatus: "Виконано")
+    ]
+    
+}
---
+++ ReFix/Views/RepairsListView.swift
@@ -0,0 +1,42 @@
+//
+//  RelpairsListView.swift
+//  ReFix
+//
+//  Created by Ivan Laptii on 23.12.2023.
+//
+
+import SwiftUI
+
+struct RelpairsListView: View {
+
+    @State var repairsArray = ResourceLoader.shared.loadAlarmList()
+
+    var body: some View {
+        List(repairsArray) { item in
+            HStack {
+                VStack(alignment: .listRowSeparatorLeading, content: {
+                    Text(item.model)
+                        .font(.system(size: 17))
+                        .padding(1)
+                    Text("#\(item.id)")
+                        .font(.system(size: 15))
+                })
+                Spacer()
+                HStack {
+                    Text("Detail")
+                        .font(.system(size: 17))
+                        .font(.system(.title3))
+                        .foregroundStyle(.secondary)
+                    Image(systemName: "chevron.right")
+                }
+            }
+            .padding(1)
+        }
+    }
+}
+
+#Preview {
+    RelpairsListView()
+}
+
+
