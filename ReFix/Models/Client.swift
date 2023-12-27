97284fe0d7e1a4a1e9301e5e710eb86cdc02d2f2
vanyalaptii <vanya.lapty@gmail.com>
2023-12-28 00:55:16 +0200

    Some Fixes

---
+++ ReFix/Models/Client.swift
@@ -0,0 +1,23 @@
+//
+//  Client.swift
+//  ReFix
+//
+//  Created by Ivan Laptii on 25.12.2023.
+//
+
+import Foundation
+
+class Client {
+    let id: Int
+    var name: String
+    var phoneNumber: String
+    var email: String
+    
+    init(id: Int, name: String, phoneNumber: String, email: String) {
+        self.id = id
+        self.name = name
+        self.phoneNumber = phoneNumber
+        self.email = email
+    }
+}
+
---
+++ ReFix/Views/RepairsListView.swift
@@ -0,0 +1,44 @@
+//
+//  RelpairsListView.swift
+//  ReFix
+//
+//  Created by Ivan Laptii on 23.12.2023.
+//
+
+import SwiftUI
+
+struct RelpairsListView: View {
+
+    @State var repairsArray = Repair.repairsMocked
+
+    var body: some View {
+        List(repairsArray) { item in
+            HStack {
+                VStack(alignment: .listRowSeparatorLeading, content: {
+                    Text(item.model)
+                        .font(.system(size: 17))
+                        .padding(1)
+                    Text("#\(item.id)")
+                        .font(.system(size: 15))
+                        .foregroundStyle(.secondary)
+                })
+                Spacer()
+                HStack {
+                    Text("Detail")
+                        .font(.system(size: 17))
+                        .font(.system(.title3))
+                        .foregroundStyle(.secondary)
+                    Image(systemName: "chevron.right")
+                        .opacity(0.5)
+                }
+            }
+            .padding(1)
+        }
+    }
+}
+
+#Preview {
+    RelpairsListView()
+}
+
+
