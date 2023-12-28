15353f69f7f04fa73c08e980dd118883d84dc28b
vanyalaptii <vanya.lapty@gmail.com>
2023-12-28 21:13:36 +0200

    Строка листу, NavigationTitle, Toolbar

--- ReFix/Views/RepairsListView.swift
+++ ReFix/Views/RepairsListView.swift
@@ -12,28 +12,56 @@ struct RelpairsListView: View {
     @State var repairsArray = Repair.repairsMocked
 
     var body: some View {
-        List(repairsArray) { item in
-            HStack {
-                VStack(alignment: .listRowSeparatorLeading, content: {
-                    Text(item.model)
-                        .font(.system(size: 17))
-                        .padding(1)
-                    Text("#\(item.id)")
-                        .font(.system(size: 15))
-                        .foregroundStyle(.secondary)
-                })
-                Spacer()
-                HStack {
-                    Text("Detail")
-                        .font(.system(size: 17))
-                        .font(.system(.title3))
-                        .foregroundStyle(.secondary)
-                    Image(systemName: "chevron.right")
-                        .opacity(0.5)
+        
+        NavigationStack{
+                List(repairsArray) { item in
+                    ZStack {
+                        HStack {
+                            VStack(alignment: .listRowSeparatorLeading, content: {
+                                Text(item.model)
+                                    .font(.system(size: 17))
+                                    .padding(1)
+                                Text("#\(item.id)")
+                                    .font(.system(size: 15))
+                                    .foregroundStyle(.secondary)
+                            })
+                            
+                            Spacer()
+                            
+                            HStack {
+                                Text("Detail")
+                                    .font(.system(size: 17))
+                                    .font(.system(.title3))
+                                    .foregroundStyle(.secondary)
+                                
+                                Image(systemName: "chevron.right")
+                                    .opacity(0.5)
+                            }
+                        }
+                        .padding(.horizontal)
+                        
+                        
+                        RoundedRectangle(cornerRadius: 10, style: .continuous)
+                            .stroke(Color(uiColor: .tertiaryLabel), lineWidth: 1)
+                    }
+                    .listRowSeparator(.hidden)
+                    
                 }
-            }
-            .padding(1)
+                .listStyle(.plain)
+                .scrollContentBackground(.hidden)
+                .navigationTitle("Ремонти")
+                .toolbarBackground(Color(.white), for: .navigationBar)
+                .toolbar {
+                    Button("+") {
+                        print("+ tapped")
+                    }
+                    .padding()
+                    .font(.largeTitle)
+                }
+                
+                
         }
+        
     }
 }
 
